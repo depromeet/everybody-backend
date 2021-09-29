b2d8d35e0e4291ab527270f2cf8e429a5cc2ce8d
Kitae Yu <yu_kitae@naver.com>
2021-09-29 22:16:17 +0900

    api call function test

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
-    log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
-func main(){
-    log.Info("hello, world")
+func main() {
+	http.Handle("/", new(testHandler))
+
+	http.ListenAndServe(":8443", nil)
 }
 
+type testHandler struct {
+	http.Handler
+}
+
+func (h *testHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Info("called! -> " + req.URL.Path)
+
+	// GET 호출
+	resp, err := http.Get("http://localhost:8888/testapi") // for test...
+	if err != nil {
+		log.Error(err)
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// 결과 출력
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err)
+		panic(err)
+	}
+
+	str := string(data)
+	log.Debug(str)
+
+	w.Write([]byte(str))
+}
