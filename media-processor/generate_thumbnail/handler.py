d025c21c3a70dbf84743ed99b3a20674fbb53c51
Jinsu Park <dev.umijs@gmail.com>
2021-10-15 11:14:29 +0900

    feat: 간단한 썸네일 생성

--- media-processor/generate_thumbnail/handler.py
+++ media-processor/generate_thumbnail/handler.py
@@ -1,50 +1,45 @@
-import cv2, os
-import numpy as np
-import requests
-import base64
+import io
+from urllib import parse
+import boto3
+from PIL import Image
+# numpy를 쓰는 이상 boto3를 또 dependency로 업로드하면
+# 제한인 250MB를 넘어가려한다... ㅜㅜ
 
+INPUT_OBJECT_PREFIX = 'original/'
+# output path가 input path와 동일하면
+# lambda 무한 loop trigger가 걸리니 절대 유의
+OUTPUT_BUCKET_NAME = 'everybody-upload-output-dev-1'
+OUTPUT_OBJECT_PREFIX = 'resized/'
 
 def handle(event, context):
-    img_array = []
-    tmp_s3_url = "https://tmp-upload-1111.s3.ap-northeast-2.amazonaws.com"
-    input_images_key = sample_images_list()
-    print(input_images_key)
+    source_bucket_name = event['Records'][0]['s3']['bucket']['name']
 
-    for image_key in input_images_key:
-        r = requests.get("/".join((tmp_s3_url, image_key)))
-        print(r.status_code)
-        encoded_img = np.fromstring(r.content, dtype=np.uint8)
-        img = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
-        height, width, layers=img.shape
-        size=(width, height)
-        img_array.append(img)
+    # key는 url encoding 되어 전송될 수 있다. 한글이나 특문, 공백 등등에서는 url encoding 된 채로
+    # s3에 접근하면 올바르게 object를 찾을 수 없다.
+    source_object_key = parse.unquote(event['Records'][0]['s3']['object']['key'])
+    print(f'썸네일 생성 for {source_object_key}')
+    filename = source_object_key[len(INPUT_OBJECT_PREFIX):]
+    s3 = boto3.resource('s3')
 
-    # os.chdir('/tmp')
-    # 'video-demo.mp4'라는 filename 생성(/tmp에 mp4 생성), 0.5frame/sec
-    output=cv2.VideoWriter('/tmp/video-demo.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 0.5, size)
-    os.system('ls /tmp')
-    for i in range(len(img_array)):
-        output.write(img_array[i])
-    output.release()
+    obj = s3.Object(source_bucket_name, source_object_key)
+    data = obj.get()['Body'].read()
+    output = generate_thumbnail(data)
 
-    # 만든 mp4 파일 자체를 rb로 열어서 payload로 보내준다.
-    mp4_file = open('/tmp/video-demo.mp4', 'rb')
+    output_bucket = s3.Bucket(OUTPUT_BUCKET_NAME)
+    for size, img in output.items():
+        tmp_file = io.BytesIO()
+        img.save(tmp_file, 'png', optimize=True)
+        output_bucket.put_object(Body=tmp_file.getvalue(), Key=f'{OUTPUT_OBJECT_PREFIX}{size}/{filename}', ContentType='image/png')
 
-    # S3에 업로드하지 않고 /tmp의 내용을 바로 base64를 통해 encode, decode하고
-    # Content-Type을 정의함으로써 binary data를 전달할 수 있음.
-    response = {
-        "headers": {
-            "Content-Type": "video/mp4",
-        },
-        'isBase64Encoded': True,
-        "statusCode": 200,
-        "body": base64.b64encode(mp4_file.read()).decode('utf-8'),
-    }
+def generate_thumbnail(data) -> dict:
+    img = Image.open(io.BytesIO(data))
+    output = dict()
+    for size in (32, 256, 1024):
+        w = max(size, img.width)
+        h = img.height * w // img.width
+        resized = img.resize((w, h))
+        output[str(size)] = resized
 
-    return response
-
-
-def sample_images_list():
-    tmp_image_keys = ["sample_images_09.jpg", "sample_images_10.jpg", "sample_images_11.jpg", "sample_images_12.jpg"]
-
-    return tmp_image_keys
+    # output은
+    # key: str, value Image.Image
+    return output
