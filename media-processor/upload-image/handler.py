40bf40d9695956fcfa214cc8e23abbe53955af55
Jinsu Park <dev.umijs@gmail.com>
2021-10-18 23:02:41 +0900

    refactor: 개별 dependency, 필요 없는 lambda 파일 삭제

--- media-processor/upload-image/handler.py
+++ media-processor/upload-image/handler.py
@@ -2,14 +2,11 @@ import io
 from urllib import parse
 import boto3
 from PIL import Image
-# numpy를 쓰는 이상 boto3를 또 dependency로 업로드하면
-# 제한인 250MB를 넘어가려한다... ㅜㅜ
 
-INPUT_OBJECT_PREFIX = 'original/'
-# output path가 input path와 동일하면
-# lambda 무한 loop trigger가 걸리니 절대 유의
 OUTPUT_BUCKET_NAME = 'everybody-upload-output-dev-1'
 OUTPUT_OBJECT_PREFIX = 'resized/'
+# api gateway body => S3 upload
+# 참고: https://gist.github.com/tomfa/87947d2773b60fc3797491d6ef5e3d0e
 
 def handle(event, context):
     source_bucket_name = event['Records'][0]['s3']['bucket']['name']
