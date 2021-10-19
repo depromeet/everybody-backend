855f9009d42a6e26ccacee8cc7c1b0330cd76db5
Jinsu Park <dev.umijs@gmail.com>
2021-10-19 15:28:45 +0900

    feat: POST multipart로 이미지 업로드 시 리사이즈 후 업로드

---
+++ uploader/handler.py
@@ -0,0 +1,110 @@
+import base64
+import io
+import json
+import traceback
+import uuid
+import boto3
+from PIL import Image
+from requests_toolbelt.multipart import decoder
+
+OUTPUT_BUCKET_NAME = 'everybody-upload-output-dev-1'
+OUTPUT_OBJECT_PREFIX = 'image/'
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(OUTPUT_BUCKET_NAME)
+
+class Multipart:
+    def __init__(self, content, content_type, name, filename):
+        self.content = content
+        self.content_type = content_type
+        self.name = name
+        self.filename = filename
+
+# api gateway body => S3 upload
+# 참고: https://gist.github.com/tomfa/87947d2773b60fc3797491d6ef5e3d0e
+# https://devlog-wjdrbs96.tistory.com/331
+def handle(event, context):
+    try:
+        print(event)
+        user = event['headers']['user']
+        body = base64.b64decode(event['body'])
+        print('body', body)
+        boundary = event['headers']['content-type']
+        print('boundary', boundary)
+        multipart_body = decoder.MultipartDecoder(body, boundary)
+
+        keys = []
+
+        for item in multipart_body.parts:
+            print('item', item)
+            print('item.headers', item.headers)
+
+            disposition = item.headers[b'Content-Disposition']
+            params = {}
+            # 참고: https://stackoverflow.com/a/6224nContent-Disposition4172/9471220
+            for disposition_part in str(disposition).split(';'):
+                kv = disposition_part.split('=', 2)
+                params[str(kv[0]).strip()] = str(kv[1]).strip('\"\'\t \r\n') if len(kv) > 1 else str(kv[0]).strip()
+            content_type = item.headers[b'Content-Type'].decode('utf-8') if b'Content-Type' in item.headers else None
+
+            part = Multipart(item.content, content_type, params.get('name'), params.get('filename'))
+            if part.name == 'image':
+                key = str(uuid.uuid4())
+                print(f'{part.filename}을 업로드합니다.')
+                resized_result = generate_thumbnail(part.content)
+                for w, img in resized_result.items():
+                    tmp_file = io.BytesIO()
+                    img.save(tmp_file, 'png', optimize=True)
+                    bucket.put_object(
+                        # ACL='public-read', # public 권한 필요
+                        ContentType='image/png',
+                        Key=f'{user}/{OUTPUT_OBJECT_PREFIX}{w}/{key}',
+                        Body=tmp_file.getvalue(),
+                    )
+                keys.append(key)
+
+            else:
+                print(f'필드명이 image가 아님: {part.name}')
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'data': {
+                    'keys': keys,
+                },
+                'error': None
+            })
+        }
+    
+    except Exception as e:
+        traceback.print_exc()
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': {
+                'keys': None,
+                'error': str(e)
+            }
+        }
+
+# 이미지 데이터를 받아 width와 사진의 dict로 리턴
+def generate_thumbnail(data) -> dict:
+    img = Image.open(io.BytesIO(data))
+    output = dict()
+    for w, h in ((48, 64), (192, 256), (768, 1024)):
+        resized = img.resize((w, h))
+        output[w] = resized
+
+    # output은
+    # key: str, value Image.Image
+    return output
+
+# 로컬 개발용
+if __name__ == '__main__':
+    from sample_data import sample_data
+    print(handle(sample_data, {}))
---
+++ uploader/sample_data.py
@@ -0,0 +1,41 @@
+# file upload 시에
+# API gateway가 lambda에게 보내는 event
+sample_data = {
+    'version': '2.0',
+    'routeKey': 'POST /upload',
+    'rawPath': '/upload',
+    'rawQueryString': '',
+    'headers': {
+        'user': 10,
+        'accept': '*/*',
+        'accept-encoding': 'gzip,deflate,br',
+        'cache-control': 'no-cache',
+        'content-length': '702',
+        'content-type': 'multipart/form-data; boundary=--------------------------389603159367705259395659',
+        'host': 'abbi52l09j.execute-api.ap-northeast-2.amazonaws.com',
+        'postman-token': 'c96333a8-e789-4b1f-aeda-c86c4b8cf51f',
+        'user-agent': 'PostmanRuntime/7.26.8',
+        'x-amzn-trace-id': 'Root=1-616e31dc-47c5a0452694876b1af2a409',
+        'x-forwarded-for': '124.50.93.166',
+        'x-forwarded-port': '443',
+        'x-forwarded-proto': 'https'
+    },
+    'requestContext': {
+        'accountId': '070251821212',
+        'apiId': 'abbi52l09j',
+        'domainName': 'abbi52l09j.execute-api.ap-northeast-2.amazonaws.com',
+        'domainPrefix': 'abbi52l09j',
+        'http': {'method': 'POST',
+        'path': '/upload',
+        'protocol': 'HTTP/1.1',
+        'sourceIp': '124.50.93.166',
+        'userAgent': 'PostmanRuntime/7.26.8'},
+        'requestId': 'Hby6ghgNIE0EJXg=',
+        'routeKey': 'POST /upload',
+        'stage': '$default',
+        'time': '19/Oct/2021:02:47:56 +0000',
+        'timeEpoch': 1634611676671
+    },
+    'body': 'LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTM4OTYwMzE1OTM2NzcwNTI1OTM5NTY1OQ0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJpbWFnZSI7IGZpbGVuYW1lPSJTY3JlZW5zaG90IGZyb20gMjAyMS0wNi0yMCAxMy01NC0xNC5wbmciDQpDb250ZW50LVR5cGU6IGltYWdlL3BuZw0KDQqJUE5HDQoaCgAAAA1JSERSAAABLQAAAEsIBgAAACU50R0AAAAEc0JJVAgICAh8CGSIAAAAGXRFWHRTb2Z0d2FyZQBnbm9tZS1zY3JlZW5zaG907wO/PgAAAERpVFh0Q3JlYXRpb24gVGltZQAAAAAAMjAyMeuFhCAwNuyblCAyMOydvCAo7J28KSDsmKTtm4QgMDHsi5wgNTTrtoQgMTbstIhsvmx6AAABFUlEQVR4nO3UQQ0AIRDAwONkrH+foIEXaTKjoK+umdkfQMT/OgDghmkBKaYFpJgWkGJaQIppASmmBaSYFpBiWkCKaQEppgWkmBaQYlpAimkBKaYFpJgWkGJaQIppASmmBaSYFpBiWkCKaQEppgWkmBaQYlpAimkBKaYFpJgWkGJaQIppASmmBaSYFpBiWkCKaQEppgWkmBaQYlpAimkBKaYFpJgWkGJaQIppASmmBaSYFpBiWkCKaQEppgWkmBaQYlpAimkBKaYFpJgWkGJaQIppASmmBaSYFpBiWkCKaQEppgWkmBaQYlpAimkBKaYFpJgWkGJaQIppASmmBaSYFpBiWkCKaQEppgWkmBaQYlpAimkBKQcgowHjzv5XlgAAAABJRU5ErkJggg0KLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLTM4OTYwMzE1OTM2NzcwNTI1OTM5NTY1OS0tDQo=',
+    'isBase64Encoded': True
+}
