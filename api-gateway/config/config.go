c45bc51ae7d678efbb905584771c64bde330c240
Jinsu Park <dev.umijs@gmail.com>
2021-10-30 20:18:57 +0900

    refactor: 컨피그 설정 방식 변경

--- api-gateway/config/config.go
+++ api-gateway/config/config.go
@@ -2,42 +2,37 @@ package config
 
 import (
 	"errors"
+	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-var (
-	STAGES = []string{"default", "local", "dev", "prd", "test"}
-)
-
 func init() {
 	Config = &config{}
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("config")
+	viper.AddConfigPath(os.Getenv("EVERYBODY_REST_CONFIG_PATH"))
+	env := strings.ToLower(os.Getenv("EVERYBODY_ENVIRONMENT"))
+	if len(env) == 0 {
+		log.Warningf("어떤 환경을 이용해 서버를 띄울지 선택해주세요. e.g. EVERYBODY_ENVIRONMENT=local")
+	}
+
 	viper.SetEnvPrefix("EVERYBODY")
 	viper.AutomaticEnv()
 
-	found := false
-
-	for _, stage := range STAGES {
-		viper.SetConfigName(stage)
-		if err := viper.ReadInConfig(); err != nil {
-			if !errors.As(err, &viper.ConfigFileNotFoundError{}) {
-				log.Fatal(err)
-			}
-		} else {
-			found = true
-			log.Infof("%s stage에 대한 설정파일을 발견했습니다.", stage)
-
-			if err := viper.Unmarshal(Config); err != nil {
-				log.Fatal(err)
-			}
+	viper.SetConfigName(env)
+	if err := viper.ReadInConfig(); err != nil {
+		if errors.As(err, &viper.ConfigFileNotFoundError{}) {
+			log.Warningf("설정파일을 하나도 찾지 못했습니다. 올바른 환경을 설정하시고, 그에 대한 설정파일을 생성해주세요.")
+		} else{
+			log.Fatal("%#v", err)
 		}
-	}
+	} else {
+		log.Infof("%s 환경 대한 설정파일을 발견했습니다.", env)
 
-	if !found {
-		log.Fatalf("설정파일을 하나도 찾지 못했습니다. 다음 stage 중 하나에 대한 설정파일을 생성해주세요. %#v", STAGES)
+		if err := viper.Unmarshal(Config); err != nil {
+			log.Fatal(err)
+		}
 	}
 	log.Info(Config)
 }
