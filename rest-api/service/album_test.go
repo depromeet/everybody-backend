efef237501c8add2bcad7407395ae92873143bb4
Jinsu Park <dev.umijs@gmail.com>
2021-11-12 21:27:33 +0900

    test: 앨범 수정 관련 테스트

--- rest-api/service/album_test.go
+++ rest-api/service/album_test.go
@@ -78,3 +78,64 @@ func TestAlbumServiceGet(t *testing.T) {
 
 	// TODO: error test
 }
+
+func TestAlbumService_UpdateAlbum(t *testing.T) {
+	t.Run("본인의 앨범 수정", func(t *testing.T) {
+		albumSvc := initializeAlbumService(t)
+		album := &ent.Album{
+			ID:   1,
+			Name: "기존 이름",
+			Edges: ent.AlbumEdges{
+				User: &ent.User{ID: 1},
+			},
+		}
+		body := &dto.UpdateAlbumRequest{
+			Name: "바뀐 이름",
+		}
+
+		albumRepo.On("Get", mock.AnythingOfType("int")).Return(album, nil)
+		albumRepo.On("Update", mock.AnythingOfType("int"), mock.AnythingOfType("*ent.Album")).Run(func(args mock.Arguments) {
+			toUpdate := args.Get(1).(*ent.Album)
+			album.Name = toUpdate.Name
+		}).Return(album, nil)
+
+		updated, err := albumSvc.UpdateAlbum(1, 1, body)
+		assert.NoError(t, err)
+		assert.Equal(t, updated.Name, "바뀐 이름")
+	})
+
+	t.Run("에러) 남의의 앨범 수정", func(t *testing.T) {
+		albumSvc := initializeAlbumService(t)
+		album := &ent.Album{
+			ID:   1,
+			Name: "기존 이름",
+			Edges: ent.AlbumEdges{
+				User: &ent.User{ID: 2},
+			},
+		}
+		body := &dto.UpdateAlbumRequest{
+			Name: "바뀐 이름",
+		}
+
+		albumRepo.On("Get", mock.AnythingOfType("int")).Return(album, nil)
+
+		updated, err := albumSvc.UpdateAlbum(1, 1, body)
+		assert.ErrorIs(t, err, ForbiddenError)
+		assert.Nil(t, updated)
+	})
+
+	t.Run("에러) 존재하지 않는 앨범 수정", func(t *testing.T) {
+		albumSvc := initializeAlbumService(t)
+
+		body := &dto.UpdateAlbumRequest{
+			Name: "바뀐 이름",
+		}
+
+		albumRepo.On("Get", mock.AnythingOfType("int")).Return(nil, &ent.NotFoundError{})
+
+		updated, err := albumSvc.UpdateAlbum(1, 1, body)
+		assert.NotNil(t, err)
+		assert.True(t, ent.IsNotFound(err))
+		assert.Nil(t, updated)
+	})
+}
