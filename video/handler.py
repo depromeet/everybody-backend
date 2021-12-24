988a5ce19359d76754f9314c25767f7abeef6345
Jinsu Park <dev.umijs@gmail.com>
2021-12-24 16:43:56 +0900

    feat: video에서 타입을 통해 좀 더 안정적으로 작업

--- video/handler.py
+++ video/handler.py
@@ -3,10 +3,11 @@ import traceback
 
 import cv2, os
 import numpy as np
-import requests
+import json
 import base64
 import boto3
 from botocore.exceptions import ClientError
+from typing import List
 
 INPUT_BUCKET_NAME = 'everybody-upload-output-dev-1'
 AUTH_HEADER = 'user'
@@ -15,15 +16,8 @@ s3 = boto3.resource('s3')
 bucket = s3.Bucket(INPUT_BUCKET_NAME)
 
 def handle(event, context):
-    img_array = []
     print(event)
-    # for 개발 과정
-    # input_images_key = sample_images_list()
-
-    # for 배포
-    input_images_key = get_image_list_from_event(event)
-    print(input_images_key)
-
+    download_video_request = DownloadVideoRequest(**json.loads(event['body']))
     size = (768, 1024) # 기본 size
     user = get_auth_user(event)
     if not user:
@@ -35,20 +29,19 @@ def handle(event, context):
             'statusCode': 401,
             'body': '유저 정보를 입력해주세요.',
         }
-
-    for image_key in input_images_key:
-        s3_absolute_key = f'{user}/image/768/{image_key}'
-        print(s3_absolute_key)
+    images = []
+    for k in download_video_request.keys:
+        s3_absolute_key = f'{user}/image/768/{k}'
+        print(f's3_absolute_key: {s3_absolute_key}')
         obj = bucket.Object(s3_absolute_key)
         try:
-
             # 참고: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Object.get
             encoded_img = np.fromstring(obj.get()['Body'].read(), dtype=np.uint8)
             img = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
             # size는 (768, 1024)로 고정하는게 편함
             # height, width, layers = img.shape
             # size = (width, height)
-            img_array.append(img)
+            images.append(img)
         except ClientError as e:
             if e.response['Error']['Code'] == 'NoSuchKey':
                 return {
@@ -67,8 +60,8 @@ def handle(event, context):
     # 'video-demo.mp4'라는 filename 생성(/tmp에 mp4 생성), X frame/sec
     output = cv2.VideoWriter('/tmp/video-demo.mp4', cv2.VideoWriter_fourcc(*'mp4v'), get_fps(event), size)
     os.system('ls /tmp')
-    for i in range(len(img_array)):
-        output.write(img_array[i])
+    for image in images:
+        output.write(image)
     output.release()
 
     # 만든 mp4 파일 자체를 rb로 열어서 payload로 보내준다.
@@ -85,25 +78,13 @@ def handle(event, context):
         "body": base64.b64encode(mp4_file.read()).decode('utf-8'),
     }
 
+    mp4_file.close()
+
     return response
 
 
-def sample_images_list():
-    tmp_image_keys = ["sample_images_09.jpg", "sample_images_10.jpg", "sample_images_11.jpg", "sample_images_12.jpg"]
-
-    return tmp_image_keys
-
-def get_image_list_from_event(event):
-    if 'body' not in event:
-        raise RuntimeError('body를 정확히 전달해주세요.')
-    body = json.loads(event['body'])
-    keys = body['keys']
-    image_list = []
-    for k in keys:
-        if 'key' not in k:
-            raise RuntimeError('사진을 식별하기 위한 key를 설정해주세요.')
-        image_list.append(k['key'])
-    return image_list
+def sample_download_video_request():
+    return DownloadVideoRequest(["sample_images_09.jpg", "sample_images_10.jpg", "sample_images_11.jpg", "sample_images_12.jpg"], None)
 
 def get_auth_user(event):
     return event.get('headers').get(AUTH_HEADER)
@@ -114,3 +95,9 @@ def get_fps(event):
     body = json.loads(event['body'])
     # duration 단위는 초
     return 1 / float(body.get('duration', '0.25'))
+
+class DownloadVideoRequest:
+    def __init__(self, keys: List[str], duration: float):
+        self.keys = keys if keys is not None else []
+        self.duration = duration if duration is not None else 0.25
+
