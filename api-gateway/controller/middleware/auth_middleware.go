86acf6306b08d5beffd538c9678fe93a863d11a0
atg0831 <atg950831@gmail.com>
2022-01-07 19:12:15 +0900

    fix: Fix auth middlewares

--- api-gateway/controller/middleware/auth_middleware.go
+++ api-gateway/controller/middleware/auth_middleware.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"net/http"
 	"strings"
 
+	"github.com/depromeet/everybody-backend/api-gateway/config"
 	"github.com/depromeet/everybody-backend/api-gateway/util"
 	"github.com/labstack/echo/v4"
 	log "github.com/sirupsen/logrus"
@@ -11,13 +13,13 @@ import (
 // 소셜 로그인 할때 클라이언트로부터 access token을 받아서 next handler로 넘겨주는 middleware
 func OauthTokenMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		headers := c.Request().Header["Authorization"]
-		if len(headers) == 0 {
+		header := c.Request().Header.Get("Authorization")
+		if len(header) == 0 {
 			log.Error("Oauth token이 존재하지 않습니다.")
 			return c.JSON(403, "Invalid...")
 		}
 
-		bearerToken := strings.Split(headers[0], "Bearer ")
+		bearerToken := strings.Split(header, "Bearer ")
 		if len(bearerToken) == 2 {
 			c.Set("oauthtoken", strings.TrimSpace(bearerToken[1]))
 		}
@@ -28,25 +30,32 @@ func OauthTokenMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 }
 
 // TODO: 나중에 token 검증하는 부분 전부 다 middleware로 빼야함...
-func AuthVerifyToken(next echo.HandlerFunc) echo.HandlerFunc {
+func AuthVerifyTokenMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		headers := c.Request().Header["Authorization"]
-		if len(headers) == 0 {
-			log.Error("jwt token이 존재하지 않습니다.")
-			return c.JSON(403, "Invalid...")
+		token := c.Request().Header.Get("Authorization")
+		if token != "" {
+			c.Request().Header.Del("Authorization")
 		}
 
-		bearerToken := strings.Split(headers[0], "Bearer ")
-		if len(bearerToken) == 2 {
-			userId, err := util.VerifyAccessToken(headers[0])
-			if err != nil {
-				log.Error("토큰 인가 실패")
-				return c.JSON(403, err.Error())
+		// 인증절차 수행
+		userId := 0
+		if config.Config.ApiGw.AuthEnable {
+			if token == "" {
+				log.Error("Token not exist...")
+				return c.String(http.StatusUnauthorized, "Token not exist...")
 			}
+			id, err := util.VerifyAccessToken(token)
+			if err != nil {
+				log.Error("Token invalid... err=", err)
+				return c.String(http.StatusForbidden, "Token invalid...")
+			}
+			userId = id
 
-			c.Set("user_id", userId)
+		} else {
+
+			log.Warn("Auth DISABLED... func 'forwardToRestApi' processing with userId=0")
 		}
-
+		c.Set("user", userId)
 		next(c)
 		return nil
 	}
